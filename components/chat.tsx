bfd955ff39a03b7882def3a3f6cd8be7080911ff
iamyare <yarephotographer@gmail.com>
2024-07-17 16:13:56 -0600

    chore: Update Chat component to use form validation and submission logic

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,47 +1,90 @@
 'use client'
 
-import { Input } from "@/components/ui/input"
+import { Input } from '@/components/ui/input'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage
+} from './ui/form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  username: z.string()
+})
 
 export default function Chat() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: ''
+    }
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    <section className="w-full h-full bg-slate-400 relative px-4">
-        <header className=" w-full py-2 px-4">
-            <h1>Chat</h1>
-        </header>
-        <aside className="flex flex-col h-full gap-10 overflow-y-auto">
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-            <p>Chat</p>
-        </aside>
+    <section className='w-full h-full  relative px-4'>
+      <header className=' w-full py-2 px-4'>
+        <h1>Chat</h1>
+      </header>
+      <aside className='flex flex-col h-full gap-10 overflow-y-auto'>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+        <p>Chat</p>
+      </aside>
 
-        <footer className="absolute w-[90%] px-2 py-1 rounded-full bg-background/50 backdrop-blur-sm bottom-4 left-1/2 -translate-x-1/2 ">
-            <div className=" relative">
-            <Input placeholder="Escribe un mensaje" className=" bg-transparent border-none" />
-            </div>
-        </footer>
-
+      <footer className='absolute w-[90%] px-2 py-1 rounded-full bg-background/50 backdrop-blur-sm bottom-4 left-1/2 -translate-x-1/2 '>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='space-y-8 relative'
+          >
+            <FormField
+              control={form.control}
+              name='username'
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder='Escribe tu mensaje' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </footer>
     </section>
   )
 }
--- components/ui/useSpeechRecognition.ts
+++ components/ui/useSpeechRecognition.ts
@@ -2,122 +2,116 @@
 import { useCallback, useEffect, useRef, useState } from "react"
 
 interface DialogEntry {
-    timestamp: string
-    text: string
+  timestamp: string
+  text: string
+}
+
+export const useSpeechRecognition = () => {
+  const [isListening, setIsListening] = useState(false)
+  const [transcript, setTranscript] = useState('')
+  const [history, setHistory] = useState<DialogEntry[]>([])
+  const recognitionRef = useRef<SpeechRecognition | null>(null)
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
+
+  const formatTimestamp = (date: Date): string => {
+    return date.toISOString()
   }
-  
-  export const useSpeechRecognition = () => {
-    const [isListening, setIsListening] = useState(false)
-    const [transcript, setTranscript] = useState('')
-    const [history, setHistory] = useState<DialogEntry[]>([])
-    const recognitionRef = useRef<SpeechRecognition | null>(null)
-    const timeoutRef = useRef<NodeJS.Timeout | null>(null)
-  
-    const formatTimestamp = (date: Date): string => {
-      return date.toISOString()
+
+  const stopListening = useCallback(() => {
+    setIsListening(false)
+    if (recognitionRef.current) {
+      recognitionRef.current.stop()
+      recognitionRef.current = null
     }
-  
-    const stopListening = useCallback(() => {
-      setIsListening(false)
-      if (recognitionRef.current) {
-        recognitionRef.current.stop()
-        recognitionRef.current = null
-      }
-      if (timeoutRef.current) clearTimeout(timeoutRef.current)
-    }, [])
-  
-    const addToHistory = useCallback((text: string) => {
-      if (text.trim() !== '') {
-        setHistory((prevHistory) => {
-          const lastEntry = prevHistory[prevHistory.length - 1]
-          if (lastEntry && lastEntry.text === text.trim()) {
-            return prevHistory // No añadir si es exactamente igual a la última entrada
-          }
-          return [
-            ...prevHistory,
-            { timestamp: formatTimestamp(new Date()), text: text.trim() }
-          ]
-        })
-        setTranscript('')
-      }
-    }, [])
-  
-    const startListening = useCallback(() => {
-      setIsListening(true)
-      const SpeechRecognition =
-        window.SpeechRecognition || window.webkitSpeechRecognition
-      const recognition = new SpeechRecognition()
-  
-      recognition.continuous = true
-      recognition.interimResults = true
-      recognition.lang = 'es-ES'
-  
-      let currentTranscript = ''
-  
-      recognition.onresult = (event: SpeechRecognitionEvent) => {
-        let interimTranscript = ''
-        let finalTranscript = ''
-  
-        for (let i = event.resultIndex; i < event.results.length; ++i) {
-          const transcript = event.results[i][0].transcript
-          if (event.results[i].isFinal) {
-            finalTranscript += transcript
-          } else {
-            interimTranscript += transcript
-          }
+    if (timeoutRef.current) clearTimeout(timeoutRef.current)
+  }, [])
+
+  const addToHistory = useCallback((text: string) => {
+    const trimmedText = text.trim()
+    if (trimmedText !== '') {
+      setHistory((prevHistory) => {
+        const lastEntry = prevHistory[prevHistory.length - 1]
+        if (lastEntry && lastEntry.text === trimmedText) {
+          return prevHistory // No añadir si es exactamente igual a la última entrada
         }
-  
-        if (finalTranscript !== '') {
-          currentTranscript += finalTranscript
-          setTranscript(currentTranscript)
+        return [
+          ...prevHistory,
+          { timestamp: formatTimestamp(new Date()), text: trimmedText }
+        ]
+      })
+      setTranscript('')
+    }
+  }, [])
+
+  const startListening = useCallback(() => {
+    if (isListening) return; // Evitar iniciar múltiples instancias
+
+    setIsListening(true)
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition
+    const recognition = new SpeechRecognition()
+
+    recognition.continuous = true
+    recognition.interimResults = true
+    recognition.lang = 'es-ES'
+
+    let currentTranscript = ''
+
+    recognition.onresult = (event: SpeechRecognitionEvent) => {
+      let interimTranscript = ''
+      let finalTranscript = ''
+
+      for (let i = event.resultIndex; i < event.results.length; ++i) {
+        const transcript = event.results[i][0].transcript
+        if (event.results[i].isFinal) {
+          finalTranscript += transcript
+        } else {
+          interimTranscript += transcript
+        }
+      }
+
+      if (finalTranscript !== '') {
+        currentTranscript += finalTranscript
+        setTranscript(currentTranscript)
+        addToHistory(currentTranscript)
+        currentTranscript = ''
+      } else {
+        setTranscript(currentTranscript + interimTranscript)
+      }
+
+      if (timeoutRef.current) clearTimeout(timeoutRef.current)
+      timeoutRef.current = setTimeout(() => {
+        if (currentTranscript !== '') {
           addToHistory(currentTranscript)
           currentTranscript = ''
-        } else {
-          setTranscript(currentTranscript + interimTranscript)
         }
-  
-        if (timeoutRef.current) clearTimeout(timeoutRef.current)
-        timeoutRef.current = setTimeout(() => {
-          if (currentTranscript !== '') {
-            addToHistory(currentTranscript)
-            currentTranscript = ''
-          }
-        }, 1000)
+      }, 1000)
+    }
+
+    recognition.onend = () => {
+      if (isListening) {
+        recognition.start()
+      } else {
+        setTranscript('')
       }
-  
-      recognition.onend = () => {
-        if (isListening) {
-          recognition.start()
-        } else {
-          setTranscript('')
-        }
-      }
-  
-      recognition.onerror = (event) => {
-        console.error('Speech recognition error', event.error)
-        if (isListening) {
-          recognition.stop()
-          setTimeout(() => recognition.start(), 1000) // Reiniciar después de 1 segundo
-        }
-      }
-  
-      recognition.start()
-      recognitionRef.current = recognition
-  
-      return () => {
-        if (timeoutRef.current) clearTimeout(timeoutRef.current)
-        recognition.stop()
-        setIsListening(false)
-      }
-    }, [isListening, addToHistory, stopListening])
-  
-    useEffect(() => {
-      return () => {
-        if (timeoutRef.current) clearTimeout(timeoutRef.current)
-        if (recognitionRef.current) recognitionRef.current.stop()
-      }
-    }, [])
-  
-    return { isListening, transcript, history, startListening, stopListening }
-  }
-  
+    }
+
+    recognition.onerror = (event) => {
+      console.error('Speech recognition error', event.error)
+      stopListening()
+      setTimeout(() => startListening(), 1000) // Reiniciar después de 1 segundo
+    }
+
+    recognition.start()
+    recognitionRef.current = recognition
+  }, [isListening, addToHistory, stopListening])
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current)
+      if (recognitionRef.current) recognitionRef.current.stop()
+    }
+  }, [])
+
+  return { isListening, transcript, history, startListening, stopListening }
+}
