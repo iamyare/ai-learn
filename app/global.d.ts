645115d22f4a4b9e78a764e9e633eb2278ebbcc4
iamyare <yarephotographer@gmail.com>
2024-07-20 14:37:27 -0600

    feat: Agregar carga perezosa para imágenes y actualizar dependencias de Supabase

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,27 +1,13 @@
+import { type Database as DB } from '@/lib/database.types'
+
 declare global {
+  type Database = DB
+  
   interface Window {
     SpeechRecognition: typeof SpeechRecognition
     webkitSpeechRecognition: typeof SpeechRecognition
   }
 
-  interface DialogEntry {
-    timestamp: string
-    text: string
-  }
-
-  interface SpeechRecognitionContextType {
-    isListening: boolean
-    transcript: string
-    history: DialogEntry[]
-    startListening: () => void
-    stopListening: () => void
-  }
-
-  type MessageType = {
-    content: string
-    isUser: boolean
-    timestamp: string
-  }
 
 
 }
