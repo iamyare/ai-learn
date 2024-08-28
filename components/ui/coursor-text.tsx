45d6fef3a5f54d59ecab2c6d8bd8863485f79579
Yamir Alejandro Rodas Elvir <79547618+iamyare@users.noreply.github.com>
2024-08-28 17:39:25 -0600

    feat: Agregar carga perezosa para imágenes en el componente de menú de usuario

--- components/ui/coursor-text.tsx
+++ components/ui/coursor-text.tsx
@@ -2,7 +2,7 @@
 
 export function CoursorText() {
   return (
-    <div className='relative inline-block h-4 w-3 ml-1 '>
+    <div className='relative inline-block h-4 w-3 ml-1'>
       <span className='absolute h-4 w-[1.5px] bg-primary animate-pulse z-[1] rounded-md'></span>
       <div className='absolute size-2 top-1/2 -translate-y-1/2 left-0 -translate-x-1/2 blur-sm bg-primary/20 animate-pulse '></div>
       <div className='absolute size-4 top-1/2 -translate-y-1/2 left-0 -translate-x-1/2 blur-md bg-primary/50 animate-pulse '></div>
