9332924567e11fc5f30c12273ee923f29a2b68f1
Yamir Alejandro Rodas Elvir <79547618+iamyare@users.noreply.github.com>
2024-09-04 16:45:58 -0600

    feat: Actualizar fuentes y estilos en el layout de la aplicación

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,15 +6,16 @@ import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
-const libreBaskerville = Libre_Baskerville({ 
+const libreBaskerville = Libre_Baskerville({
   weight: ['400', '700'],
   subsets: ['latin'],
-  variable: '--font-libre-baskerville',
+  variable: '--font-libre-baskerville'
 })
 
 export const metadata: Metadata = {
   title: 'Stick Note',
-  description: 'Aplicación de Notebooks Inteligentes que integra PDFs, transcripción en tiempo real y chat con IA para mejorar la productividad.'
+  description:
+    'Aplicación de Notebooks Inteligentes que integra PDFs, transcripción en tiempo real y chat con IA para mejorar la productividad.'
 }
 
 export const viewport: Viewport = {
@@ -22,7 +23,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false
-};
+}
 
 export default function RootLayout({
   children
@@ -31,14 +32,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='es' suppressHydrationWarning>
-          <head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
-        <link rel="apple-touch-icon" href="/icon-512x512.png" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      <head>
+        <link rel='manifest' href='/manifest.json' />
+        <meta name='theme-color' content='#000000' />
+        <link rel='apple-touch-icon' href='/icon-512x512.png' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta
+          name='apple-mobile-web-app-status-bar-style'
+          content='black-translucent'
+        />
       </head>
-      <body className={`${inter.variable} ${libreBaskerville.variable} font-sans`}>
+      <body
+        className={`${inter.variable} ${libreBaskerville.variable} font-sans`}
+      >
         <ThemeProvider
           attribute='class'
           defaultTheme='system'
