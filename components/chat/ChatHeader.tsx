e53184d8900d7cc14b9a3635d890b695997b5928
Yamir Alejandro Rodas Elvir <79547618+iamyare@users.noreply.github.com>
2024-09-08 17:10:38 -0600

    feat: Actualizar fuentes y estilos en el layout de la aplicación
    Actualizar fuentes y estilos en el layout de la aplicación para mejorar la apariencia y la legibilidad de la interfaz.
    
    ---
    
    chore: Actualizar dependencia next-pwa a la versión 5.6.0
    
    Actualizar la dependencia next-pwa a la versión 5.6.0 para aprovechar las últimas mejoras y correcciones de errores.
    
    ---
    
    feat: Mejorar gráficos y formato de ejes en el componente Chart
    
    Realizar mejoras en los gráficos y el formato de los ejes en el componente Chart para proporcionar una representación visual más clara de los datos.
    
    ---
    
    feat: Agregar funcionalidad de descarga de chat en el encabezado del chat
    
    Agregar la funcionalidad de descarga de chat en el encabezado del chat para permitir a los usuarios guardar el historial de conversación en un archivo.
    
    ---
    
    feat: Agregar funcionalidad de descarga de chat en el encabezado del chat
    
    Agregar la funcionalidad de descarga de chat en el encabezado del chat para permitir a los usuarios guardar el historial de conversación en un archivo.

--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -28,7 +28,7 @@ const ChatHeader: React.FC<ChatHeaderProps> = ({ chat }) => {
   // }
 
   return (
-    <header className='flex w-full py-2 px-4 justify-between items-center absolute top-0 left-0 z-10 bg-background/70 backdrop-blur-sm'>
+    <header className='flex w-full py-2 px-4 justify-between items-center absolute top-0 left-0 z-[1] bg-background/70 backdrop-blur-sm'>
       <h2 className='text-lg font-semibold'>Chat</h2>
 
       {/* <div className='flex items-center gap-4'>
