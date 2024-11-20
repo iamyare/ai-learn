1e21ab09b6666d61a030c9f090cbef68dc8a9586
Yamir Alejandro Rodas Elvir <79547618+iamyare@users.noreply.github.com>
2024-11-20 13:40:39 -0600

    Refactor: Añadir detección del sistema operativo para mostrar la tecla de comando adecuada en el menú de navegación

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Search, type LucideIcon } from 'lucide-react'
-
 import {
   SidebarMenu,
   SidebarMenuButton,
@@ -10,6 +9,7 @@ import {
 import Link from 'next/link'
 import { useUserStore } from '@/stores/useUserStore'
 import SearchDialog from './search-dialog'
+import { useEffect, useState } from 'react'
 
 export function NavMain({
   items
@@ -22,7 +22,18 @@ export function NavMain({
   }[]
 }) {
   const { user } = useUserStore()
+  const [commandKey, setCommandKey] = useState('Ctrl')
+
+  useEffect(() => {
+    // Detect OS and set appropriate command key
+    const isMac =
+      typeof window !== 'undefined' &&
+      /Mac|iPod|iPhone|iPad/.test(navigator.platform)
+    setCommandKey(isMac ? '⌘' : 'Ctrl')
+  }, [])
+
   if (!user) return null
+
   return (
     <SidebarMenu>
       {items.map((item) => (
@@ -36,9 +47,14 @@ export function NavMain({
         </SidebarMenuItem>
       ))}
       <SidebarMenuItem>
-        <SidebarMenuButton>
-          <Search />
-          <span>Buscar</span>
+        <SidebarMenuButton className='flex justify-between'>
+          <div className='flex items-center gap-2'>
+            <Search className='size-4' />
+            <span>Buscar</span>
+          </div>
+          <kbd className='pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded  bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-70'>
+            <span>{commandKey}</span>K
+          </kbd>
         </SidebarMenuButton>
       </SidebarMenuItem>
       <SearchDialog />
